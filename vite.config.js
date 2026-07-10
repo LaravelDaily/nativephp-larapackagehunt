facc9cabc5b452d0adc3c9c88e499e1696a243f5
krekas <krekas@gmail.com>
2026-07-10 13:07:04 +0300

    vite hot reload

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,14 @@ import {
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from "@tailwindcss/vite";
+import { nativephpMobile, nativephpHotFile } from './vendor/nativephp/mobile/resources/js/vite-plugin.js';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            hotFile: nativephpHotFile(),
             fonts: [
                 bunny('Instrument Sans', {
                     weights: [400, 500, 600],
@@ -17,6 +19,7 @@ export default defineConfig({
             ],
         }),
         tailwindcss(),
+        nativephpMobile(),
     ],
     server: {
         cors: true,
